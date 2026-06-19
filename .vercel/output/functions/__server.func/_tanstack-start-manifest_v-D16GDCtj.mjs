c85cdb6369037caf4df065e2464f53eb04fd1191
joao vitor <joaovitorevora@gmail.com>
2026-06-18 21:23:11 -0300

    Update gitignore

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D16GDCtj.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/joaov/OneDrive/Desktop/portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-Dn154BWO.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-Dn154BWO.js" } }] }, "/": { filePath: "C:/Users/joaov/OneDrive/Desktop/portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-C9J6S9k4.js"] } } });
+export {
+  tsrStartManifest
+};
